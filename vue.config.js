4eda4b7e755db9c15e9e97cc6e718d71bc1dca71
mute520 <1183832027@qq.com>
2020-12-28 15:30:36 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '././2020-new-pro' : '/',
+  // publicPath: '/',
   assetsDir: 'static',
   outputDir: 'docs',
   devServer: {
